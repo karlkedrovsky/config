e12e92dc57d5f984a65672fcb04ac0f718e62db6
Karl Kedrovsky <karl@kedrovsky.com>
2013-08-24 12:47:00 +0000

    updated path to drush-config directory

--- drush/drushrc.php
+++ drush/drushrc.php
@@ -1,11 +1,9 @@
 <?php
-// if (is_dir('/Users/kkedrovsky')) {
-//   $_SERVER['DRUPAL_CONFIG'] = '/Users/kkedrovsky/drupal-config/';
-//   $options['dump-dir'] = '/Users/kkedrovsky/drush-dump-dir';
-// } elseif (is_dir('/home/karl')) {
-//   $_SERVER['DRUPAL_CONFIG'] = '/home/karl/drupal-config/';
-//   $options['dump-dir'] = '/home/karl/drush-dump-dir';
-// }
+
 $home_directory = getenv('HOME');
-$_SERVER['DRUPAL_CONFIG'] = $home_directory . '/drupal-config/';
+if (is_dir('/var/drupal-config')) {
+  $_SERVER['DRUPAL_CONFIG'] = '/var/drupal-config/';
+} else {
+  $_SERVER['DRUPAL_CONFIG'] = $home_directory . '/drupal-config/';
+}
 $options['dump-dir'] = $home_directory . '/drush-dump-dir';
