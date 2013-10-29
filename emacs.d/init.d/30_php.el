b0b31fcc43b30e3d7d86b3afccb6abcd9790245a
Karl Kedrovsky <karl@kedrovsky.com>
2013-10-28 21:05:06 -0500

    Use web-mode for twig files.

--- emacs.d/init.d/30_php.el
+++ emacs.d/init.d/30_php.el
@@ -1,8 +1,9 @@
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.module$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.theme$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.twig$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\.php$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.blade\.php$" . web-mode))
 (add-hook 'php-mode-hook 'php-enable-drupal-coding-style)
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
-
