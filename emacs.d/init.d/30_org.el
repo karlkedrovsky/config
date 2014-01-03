2555de18aac5ab6c71e54b4b2ad03ce6a234af34
Karl Kedrovsky <karl@kedrovsky.com>
2014-01-03 16:15:28 -0600

    Added org file directories to agenda list.

--- emacs.d/init.d/30_org.el
+++ emacs.d/init.d/30_org.el
@@ -1,5 +1,11 @@
 (setq org-directory "~/owncloud/org")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
+
+(setq org-agenda-files (list
+                        org-directory
+                        (concat org-directory "/home")
+                        (concat org-directory "/work")))
+
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
