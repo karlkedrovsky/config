12a97effd92422c15241c3c68ed3fa5d1cf3893d
Karl Kedrovsky <karl@kedrovsky.com>
2014-12-06 09:02:35 -0600

    Updated package repos

--- emacs.d/init.d/10_packages.el
+++ emacs.d/init.d/10_packages.el
@@ -2,7 +2,7 @@
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
