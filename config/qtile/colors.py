66ff2ea7aa652b87ad817531ba19684cc94fa44e
Karl Kedrovsky <karl@kedrovsky.com>
2022-12-18 15:42:13 -0600

    initial config for nvim and qtile

---
+++ config/qtile/colors.py
@@ -0,0 +1,26 @@
+def dracula():
+    return [["#282a36", "#282a36"],  # dark grey [0] (background)
+            ["#44475a", "#44475a"],  # light grey [1]
+            ["#f8f8f2", "#f8f8f2"],  # white [2] (foreground)
+            ["#6272a4", "#6272a4"],  # blue/grey) [3]
+            ["#8be9fd", "#8be9fd"],  # cyan [4]
+            ["#50fa7b", "#50fa7b"],  # green [5]
+            ["#ffb86c", "#ffb86c"],  # orange [6]
+            ["#ff79c6", "#ff79c6"],  # pink [7]
+            ["#bd93f9", "#bd93f9"],  # purple [8]
+            ['#ff5555', '#ff5555'],  # red [9]
+            ["#f1fa8c", "#f1fa8c"]]  # yellow [10]
+
+def nord():
+    return [["#2e3440", "#2e3440"],  # dark grey [0] (background)
+            ["#4c556a", "#4c566a"],  # grey [1]
+            ["#d8dee9", "#d8dee9"],  # beige [2] (foreground)
+            ["#81a1c1", "#81a1c1"],  # blue/grey) [3]
+            ["#88c0d0", "#88c0d0"],  # blue [4]
+            ["#a3be8c", "#a3be8c"],  # green [5]
+            ["#e69875", "#e69875"],  # orange [6]
+            ["#D196B3", "#D196B3"],  # pink [7]
+            ["#a3be8c", "#a3be8c"],  # green [8]
+            ['#bf616a', '#bf616a'],  # red [9]
+            ["#ebcb8b", "#ebcb8b"]]  # yellow [10]
+
