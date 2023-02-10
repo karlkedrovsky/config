1fe6e62862b863e4c369ea6281c9d9eb599bd51f
Karl Kedrovsky <Karl.Kedrovsky@vmlyr.com>
2023-02-10 14:14:54 -0600

    fixed missing border on floating nvim windows

--- config/nvim/lua/karl/plugins/init.lua
+++ config/nvim/lua/karl/plugins/init.lua
@@ -17,7 +17,7 @@ require('packer').init({
     compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
     display = {
         open_fn = function()
-            return require('packer.util').float({ border = 'solid' })
+            return require('packer.util').float({ border = 'single' })
         end,
     },
 })
